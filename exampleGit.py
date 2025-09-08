47f1a95e48f44b4d776c7248f23592810f6b70f4
wabalaba <yarinkasai9@gmail.com>
2025-09-08 14:07:48 +0300

    first

---
+++ exampleGit.py
@@ -0,0 +1,12 @@
+#1
+print(" Hello Enter your  name: ")
+print("aya")
+#2
+print(" Hello Enter your adress: ")
+print("Amal, Givatayim")
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
